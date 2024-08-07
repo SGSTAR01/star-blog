366c2cf58a0e4cd366891eeccf75c040e92a5d53
SGSTAR01 <startroidx@gmaill.com>
2024-08-07 21:25:08 +0530

    username now showing in navbar

--- services/authentication.js
+++ services/authentication.js
@@ -6,6 +6,7 @@ const secret = process.env.JWT_SECRET;
 export function createToken(user) {
   const payload = {
     id: user._id,
+    name: user.name,
     email: user.email,
     profilePicture: user.profilePicture,
     role: user.role,
